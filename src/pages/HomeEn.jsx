6afeca5d5718c5236d97949ac17b8510c42be9fd
Rubens Neto <rubensmn96@gmail.com>
2026-02-24 18:12:42 -0300

    edited files path and removed vercel.json

--- src/pages/HomeEn.jsx
+++ src/pages/HomeEn.jsx
@@ -1,9 +1,9 @@
-import { NavbarEn } from "../en/components/NavbarEn";
-import { RubensEn } from "../en/components/RubensEn";
-import { About } from "../en/components/About";
-import { Skills } from "../en/components/Skills";
-import { Contact } from "../en/components/Contact";
-import { FooterEn } from "../en/components/FooterEn";
+import { NavbarEn } from "../components/en/NavbarEn";
+import { RubensEn } from "../components/en/RubensEn";
+import { About } from "../components/en/About";
+import { Skills } from "../components/en/Skills";
+import { Contact } from "../components/en/Contact";
+import { FooterEn } from "../components/en/FooterEn";
 
 export function HomeEn() {
     return (
